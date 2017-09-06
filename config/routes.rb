70154a91aca0614cd9cb632e52e81f2f432c02e2
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-09-06 12:09:30 -0400

    Generate user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/user_params'
+
   resources :questions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
