93d5f0a8f5a52622b1d8c477b4d7a7b8bca7bb86
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-08 15:35:15 -0400

    Fix tag controller test

--- test/controllers/tags_controller_test.rb
+++ test/controllers/tags_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TagsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get tags_index_url
+    get tags_path
     assert_response :success
   end
 
