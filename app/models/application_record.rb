3288f00077b0b81879cc2d27180e52a4ffb08bd2
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-09-11 15:26:04 -0400

    Move necessary lines into question model instead of application record

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
-  include PgSearch
   self.abstract_class = true
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
+  include PgSearch
   pg_search_scope :search_for, against: %i(title body), using: { tsearch: { any_word: true } }
   acts_as_taggable
   has_many :answers
