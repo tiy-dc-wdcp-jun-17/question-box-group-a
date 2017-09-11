877e7847de72766336f335529fa78d5221a2c8d4
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-09-11 14:42:31 -0400

    Implemented pg-search to search through questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ApplicationRecord
+  pg_search_scope :search_for, against: %i(title body), using: { tsearch: { any_word: true } }
   acts_as_taggable
   has_many :answers
   belongs_to :user
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post '/search', :to => 'search#create'
+
   get '/tags', :to => 'tags#index'
   root to: 'sessions#new'
   resource :session, only: [:new, :create, :destroy]
