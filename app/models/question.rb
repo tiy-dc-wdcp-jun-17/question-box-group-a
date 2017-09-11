439e0d00ce783b55a28f9f0549b7974a588d073d
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-11 15:11:47 -0400

    Add search feature

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ApplicationRecord
   acts_as_taggable
   has_many :answers
   belongs_to :user
+
+  pg_search_scope :search_for, against: %i(title body), using: {tsearch: { any_word: true} }
+
 end
