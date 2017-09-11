36da6aed2b27b3059748fe8b291c795a1315dc55
Carolina de la Fuente <carolina.delafuente@theironyard.com>
2017-09-11 11:56:12 -0400

    Add reference migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908183200) do
+ActiveRecord::Schema.define(version: 20170908200028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20170908183200) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.bigint "answer_id"
+    t.index ["answer_id"], name: "index_questions_on_answer_id"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
@@ -66,4 +68,5 @@ ActiveRecord::Schema.define(version: 20170908183200) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "questions", "answers"
 end
