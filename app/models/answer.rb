1ddf371b23671cd8a861576b7cc0b79d6d67b708
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-09-28 19:32:42 -0400

    added pgsearch to answers and questions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,8 @@
 class Answer < ApplicationRecord
+  include PgSearch
   belongs_to :user
   belongs_to :question
+
+  multisearchable against: %i(title body)
+  
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,9 @@
 class Question < ApplicationRecord
+  include PgSearch
   acts_as_taggable
   has_many :answers
   belongs_to :user
 
   pg_search_scope :search_for, against: %i(title body), using: {tsearch: { any_word: true} }
-
+  multisearchable against: %i(body)
 end
